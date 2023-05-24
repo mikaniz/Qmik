2aa335077b4ad8ec81f029e1d176f4edea59cd4c
mikaniz <k515jina@gmail.com>
2023-05-24 17:48:19 +0900

    feat: Create ModelGenerator class
    ModelGenerator class reads configuration xml file, reads metadata of
    configured databases, and generates model class that reflects database
    information

---
+++ src/com/qmik/model/ModelGenerator.java
@@ -0,0 +1,77 @@
+package com.qmik.model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+import com.qmik.database.Column;
+import com.qmik.database.Database;
+import com.qmik.database.MetaDataLoader;
+import com.qmik.database.Table;
+
+public class ModelGenerator {
+	
+	public static void generate(String basePath) throws IOException {
+		File file = new File("${project.basedir}/../qmik-config.xml");
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		
+		try {
+			SAXParser parser = factory.newSAXParser();
+			ConfigHandler handler = new ConfigHandler();
+			parser.parse(file, handler);
+			
+			List<Database> databases = handler.getDatabases();
+			for (Database database : databases) {
+				List<Table> tables = MetaDataLoader.load(database);
+				for (Table table : tables) generate(basePath, basePath.replaceAll("[/]", "."), database, table);
+			}
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void generate(String basePath, String basePackage, Database database, Table table) {
+		File qmikFolder = new File(new StringBuilder().append("src/main/java/").append(basePath).append("/qmik").toString());
+		if (!qmikFolder.exists()) qmikFolder.mkdir();
+		
+		File modelFolder = new File(qmikFolder, database.getId());
+		if (!modelFolder.exists()) modelFolder.mkdir();
+		
+		File model = new File(modelFolder, new StringBuilder().append(table.getName().toUpperCase()).append(".java").toString());
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("package ").append(basePackage).append(".qmik.").append(database.getId()).append(";\n\n")
+				.append("import com.qmik.query.Attribute;\n\n")
+				.append("public class ").append(table.getName().toUpperCase()).append(" {\n\n")
+				.append("\tfinal static String URL = \"").append(database.getUrl()).append("\";\n")
+				.append("\tfinal static String USER = \"").append(database.getUser()).append("\";\n")
+				.append("\tfinal static String PASSWORD = \"").append(database.getPassword()).append("\";\n")
+				.append("\tfinal static String TABLE = \"").append(table.getName()).append("\";\n\n");
+		
+		for (Column column : table.getColumns()) {
+			builder.append("\tstatic Attribute<").append(column.getType()).append("> ").append(column.getName())
+					.append(" = new Attribute<>(\"").append(column.getName()).append("\");\n");
+		}
+		
+		builder.append("\n}");
+		
+		try (FileOutputStream fileOutputStream = new FileOutputStream(model)) {
+			fileOutputStream.write(builder.toString().getBytes());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
