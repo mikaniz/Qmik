b14e8593ac9a656038505deccc414bcd8771c062
mikaniz <k515jina@gmail.com>
2023-05-25 12:08:17 +0900

    feat: Create QueryExecutor class

---
+++ src/com/qmik/database/QueryExecutor.java
@@ -0,0 +1,46 @@
+package com.qmik.database;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.qmik.query.Condition;
+import com.qmik.query.SelectQuery;
+
+public class QueryExecutor {
+	
+	public static Map<String, Object> execute(SelectQuery query) {
+		Map<String, Object> result = new HashMap<>();
+		
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = ConnectionManager.getConnection(query.driver, query.url, query.user, query.password);
+			statement = connection.prepareStatement(query.toString());
+			
+			List<Condition<?>> conditions = query.getConditions();
+			int length = conditions.size();
+			for (int i = 0; i < length; i++) statement.setObject(i + 1, conditions.get(i).getValue());
+			
+			resultSet = statement.executeQuery();
+			if (resultSet.next()) {
+				ResultSetMetaData metaData = resultSet.getMetaData();
+				int columnCount = metaData.getColumnCount();
+				for (int i = 1; i <= columnCount; i++) {
+					String column = metaData.getColumnName(i);
+					result.put(column, resultSet.getObject(column));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionManager.close(resultSet, statement, connection);
+		}
+		
+		return result;
+	}
+	
+}
